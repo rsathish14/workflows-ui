4a8165c19330e3dd127aa851f72bd922d1f69ba0
rsathish14 <sathish.ramani14@gmail.com>
2018-12-25 18:40:03 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state= {"userid": "", "name": ""};  
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);  
+  }
+
+  handleInputChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {    
+    alert('You created user: ' + this.state.userid + ' ' + this.state.name);
+    fetch('http://192.168.99.100:8081/persons/create', {
+    method: 'post',
+    body: "{userid:" + this.state.userid + ", name:" + this.state.name +"}"
+  }).then(function(response) {
+    return response.json();
+  })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>
+            Create Person
+          </h1>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              User ID:
+    <input type="text" name="userid" value={this.state.userid} onChange={this.handleInputChange}/>
+            </label>
+            <label>
+              User Name:
+    <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange}/>
+            </label>
+            <input type="submit" value="Submit"/>
+          </form>
         </header>
       </div>
     );
